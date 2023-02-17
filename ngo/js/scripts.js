5c092db9ccb42fd52c9efd8bba14b5f5ddc762e8
Jack Crosbie <jackwcrosbie@gmail.com>
2023-02-17 14:59:35 +0000

    fixed js issues with prevSlide function

--- ngo/js/scripts.js
+++ ngo/js/scripts.js
@@ -26,16 +26,16 @@ nextSlide.addEventListener("click", function () {
 const prevSlide = document.querySelector(".btn-prev");
 
 // add event listener and navigation functionality
-/* prevSlide.addEventListener("click", function () {
+prevSlide.addEventListener("click", function () {
     // check if current slide is the first and reset current slide to last
     if (curSlide === 0) {
         curSlide = maxSlide;
     } else {
-        curSlide -;
+        curSlide--;
     }
 
     //   move slide by 100%
     slides.forEach((slide, indx) => {
         slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
     });
-}); */
+});
