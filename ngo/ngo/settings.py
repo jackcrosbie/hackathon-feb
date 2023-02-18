6c56c96b11e80860a5ecdce7796cd38e55745e77
Jack Crosbie <jackwcrosbie@gmail.com>
2023-02-18 18:11:55 +0000

    add secret key values to settings.py

--- ngo/ngo/settings.py
+++ ngo/ngo/settings.py
@@ -22,11 +22,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = ('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
-# DEBUG = True
+#DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = True
 
 ALLOWED_HOSTS = ["localhost", '127.0.0.1']
 
