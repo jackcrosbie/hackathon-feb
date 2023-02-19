156d951449548b6b75882370c004437e80c23c40
Jack Crosbie <jackwcrosbie@gmail.com>
2023-02-19 14:51:12 +0000

    changed build.sh file

--- ngo/build.sh
+++ ngo/build.sh
@@ -1,5 +1,6 @@
- set -o errexit
- pip install -r requirements.txt
- python manage.py collectstatic --no-input
- python manage.py makemigrations && python manage.py migrate
+set -o errexit
+pip install -r requirements.txt
+python manage.py collectstatic --no-input --settings=ngo.settings
+python manage.py makemigrations --settings=ngo.settings
+python manage.py migrate --settings=ngo.settings
  
