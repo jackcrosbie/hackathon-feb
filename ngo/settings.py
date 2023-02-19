69a9152f6059d8e9f23958688a89d8cb1f72da06
Erik Vodopivec Forsman <02linfor01@gmail.com>
2023-02-19 22:12:38 +0000

    deploy7

--- ngo/settings.py
+++ ngo/settings.py
@@ -136,7 +136,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
