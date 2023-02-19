7e6606b6ff1bf980b20891902b37786be7ca80f7
Jack Crosbie <jackwcrosbie@gmail.com>
2023-02-19 15:40:12 +0000

    fix for deployment

--- ngo/wsgi.py
+++ ngo/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ngo.ngo.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ngo.settings')
 
 application = get_wsgi_application()
